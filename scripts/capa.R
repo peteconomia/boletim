9ef0c16ae332f402193a012c8010d14f51be2ba9
pedrocastroo <pdesacastro@gmail.com>
2020-11-24 03:57:37 -0300

    nova capa

---
+++ scripts/capa.R
@@ -0,0 +1,23 @@
+# Script para gerar figura de fundo da capa
+library(geobr)
+library(ggplot2)
+
+# remove os eixos, grade e deixa o fundo transparent
+config <- theme(
+  panel.background = element_rect(fill = "transparent", color = NA),
+  plot.background = element_rect(fill = "transparent", color = NA),
+  axis.title=element_blank(),
+  axis.text=element_blank(),
+  axis.ticks=element_blank(),
+  panel.grid.major = element_blank(), 
+  panel.grid.minor = element_blank()
+)
+
+# read_municipality é uma função do pacote geobr
+all_muni_to <- read_municipality(code_muni = "TO", year= 2010)
+
+ggplot() +
+  geom_sf(data=all_muni_to, fill="#0969a5", color="#00609D", size=.15, show.legend = FALSE) +
+  theme_minimal() +
+  config +
+  ggsave('../tex/logos/bg.pdf', height = 20, units = "cm") # salva como pdf

d306efa1c86ecc297c657aca811597456a536e80
“felipeferreiraa” <ferreirafelipee@hotmail.com>
2021-02-11 10:18:38 -0300

    update

--- scripts/PMC.R
+++ scripts/PMC.R
@@ -5,10 +5,10 @@ library(petgg)
 petgg::set_theme(base_family = "EB Garamond")
 
 pmc <- get_sidra(api = "/t/3416/n1/all/n3/17/v/564/p/last%2011/c11046/33534/d/v564%201") %>% 
-  mutate(date = parse_date(`MÍs (CÛdigo)`, format = '%Y%m')) %>% 
-  select(`Brasil e Unidade da FederaÁ„o`, Valor, date)
+  mutate(date = parse_date(`M√™s (C√≥digo)`, format = '%Y%m')) %>% 
+  select(`Brasil e Unidade da Federa√ß√£o`, Valor, date)
 
-pmc_plot <- ggplot(pmc, aes(x=date, y = Valor, fill = `Brasil e Unidade da FederaÁ„o`))+
+pmc_plot <- ggplot(pmc, aes(x=date, y = Valor, fill = `Brasil e Unidade da Federa??o`))+
   geom_bar(stat="identity", position = position_dodge(width = 25), width = 25)+
   scale_x_date(date_labels="%b",date_breaks  ="1 month")+
   geom_text(aes(label = paste(Valor),vjust = ifelse(Valor >= 0, -0.3, 1)),
